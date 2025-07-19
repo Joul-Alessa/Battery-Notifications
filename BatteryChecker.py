1db15d5149004e68fac9cbef9dc7b7203c8970a9
José Espinoza <joseluisessa@gmail.com>
2025-07-19 00:18:37 -0600

    Aplicacion transformada a servicio, falta la señal a la GUI para mostrar la notificacion en la PC

--- BatteryChecker.py
+++ BatteryChecker.py
@@ -11,121 +11,132 @@ import time
 from playsound import playsound
 from notifypy import Notify
 import requests
-import signal
 import sys
 import json
+from SMWinservice import SMWinservice
 
 # Librarías para evitar mostrar información sensible en el código
 from dotenv import load_dotenv
 import os
 
-# Carga las variables de entorno del archivo .env
-load_dotenv()
-
-# Se accede a la información del json y se guardas su infomación
-try:
-    with open('conf.json', 'r') as file:
-        args = json.load(file)
-except FileNotFoundError:
-    print("Error: The file was not found.")
-    sys.exit(1)
-except json.JSONDecodeError:
-    print("Error: The file is not a valid JSON.")
-    sys.exit(1)
-
-# Accede a las variables de entorno
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
 #%% Código
+class BatteryChecker(SMWinservice):
+    _svc_name_ = "BatteryChecker"
+    _svc_display_name_ = "Battery Notification Service"
+    _svc_description_ = "Python Service that monitors the battery level status"
+    args = {}
+    TOKEN = ""
+    CHAT_ID = ""
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Función que manda mansaje a través de Telegram
-def send_telegram_message(message):
-    if args['msgTelegram']:
-        url = "https://api.telegram.org/bot" + str(TOKEN) + "/sendMessage"
-        payload = {
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-        
+    def start(self):
+        # Carga las variables de entorno del archivo .env
+        load_dotenv(os.path.join(BatteryChecker.BASE_DIR, ".env"))
+
+        # Se accede a la información del json y se guardas su infomación
         try:
-            requests.post(url, json=payload)
-        except requests.ConnectionError as e:
-            print(f"Error de conexión: {e}")
-        except requests.Timeout as e:
-            print(f"Error de tiempo de espera: {e}")
-        except requests.RequestException as e:
-            print(f"Error general de requests: {e}")
+            with open(os.path.join(BatteryChecker.BASE_DIR, 'conf.json'), 'r') as file:
+                BatteryChecker.args = json.load(file)
+        except FileNotFoundError:
+            print("Error: The file was not found.")
+            sys.exit(1)
+        except json.JSONDecodeError:
+            print("Error: The file is not a valid JSON.")
+            sys.exit(1)
 
-# Función que manda la notificacion de sistema en PC
-def send_notification(title, message):
-    notification = Notify()
-    notification.title = title
-    notification.message = message
-    notification.send()
+        # Accede a las variables de entorno
+        BatteryChecker.TOKEN = os.getenv("TOKEN")
+        BatteryChecker.CHAT_ID = os.getenv("CHAT_ID")
+        self.isrunning = True
+    
+    def restart(self):
+        self.isrunning = False
+        self.start()
 
-# Función que reproduce el sonido para la notifiaciones de sistema en PC
-def play_notification_sound(sound_file_name):
-    if args['sound']:
-        try:
-            playsound(os.path.join(os.path.dirname(os.path.abspath(__file__)), "sounds", sound_file_name))
-        except FileNotFoundError as e:
-            print(f"Error: {e}")
-        except PermissionError:
-            print("Error: No tienes permisos para acceder al archivo.")
-        except Exception as e:
-            print(f"Ocurrió un error inesperado: {e}")
+    # Función que revisa cuando el archivo está por ser interrumpido (para las suspensiones y apagados)
+    def stop(self):
+        self.isrunning = False
+        if BatteryChecker.args['closing']:
+            battery = psutil.sensors_battery()
+            level = battery.percent
+            plugged = battery.power_plugged
+            if plugged == True:
+                messageNotif = f"ℹ️ El programa dejará de monitorear el nivel de batería ({level} %). Desenchufar"
+            else:
+                messageNotif = f"ℹ️ El programa dejará de monitorear el nivel de batería ({level} %)."
+            self.send_telegram_message(messageNotif)
 
-# Función que revisa cuando el archivo está por ser interrumpido (para las suspensiones y apagados)
-def handle_interrupt(signal_received, frame):
-    if args['closing']:
+    # ciclo para revisar el nivel de la batería
+    def main(self):
+        while self.isrunning:
+            self.check_battery_level()
+            time.sleep(10)  # Espera 10 segundos antes de volver a comprobar
+    
+    # Función que manda mansaje a través de Telegram
+    def send_telegram_message(self,message):
+        if BatteryChecker.args['msgTelegram']:
+            url = "https://api.telegram.org/bot" + str(BatteryChecker.TOKEN) + "/sendMessage"
+            payload = {
+                "chat_id": BatteryChecker.CHAT_ID,
+                "text": message
+            }
+            
+            try:
+                requests.post(url, json=payload)
+            except requests.ConnectionError as e:
+                print(f"Error de conexión: {e}")
+            except requests.Timeout as e:
+                print(f"Error de tiempo de espera: {e}")
+            except requests.RequestException as e:
+                print(f"Error general de requests: {e}")
+
+    # Función que manda la notificacion de sistema en PC
+    def send_notification(self,title, message):
+        #Señal a la GUI para disparar la notificación con su información correspondiente
+        wenas=0
+
+    # Función que reproduce el sonido para la notifiaciones de sistema en PC
+    def play_notification_sound(self,sound_file_name):
+        if BatteryChecker.args['sound']:
+            try:
+                playsound(os.path.join(BatteryChecker.BASE_DIR, "sounds", sound_file_name))
+            except FileNotFoundError as e:
+                print(f"Error: {e}")
+            except PermissionError:
+                print("Error: No tienes permisos para acceder al archivo.")
+            except Exception as e:
+                print(f"Ocurrió un error inesperado: {e}")
+            
+
+    # Función general que revisa el estado de la batería
+    def check_battery_level(self):
+        # Obtener información de la batería
         battery = psutil.sensors_battery()
-        level = battery.percent
-        plugged = battery.power_plugged
-        if plugged == True:
-            messageNotif = f"ℹ️ El programa dejará de monitorear el nivel de batería ({level} %). Desenchufar"
+        if battery is not None:
+            level = battery.percent
+            plugged = battery.power_plugged
+            
+            # Verificar el nivel de la batería y si está conectada a la corriente
+            if level <= BatteryChecker.args['lower'] and not plugged:
+                titleNotif = "Alerta de Batería"
+                messageNotif = f"⚠️ Nivel de batería bajo ({level} %). \nConecta el cargador."
+                self.send_notification(titleNotif, messageNotif)
+                self.send_telegram_message(messageNotif)
+                self.play_notification_sound("battery-low.mp3")
+                
+                time.sleep(80)
+            elif level >= BatteryChecker.args['higher'] and plugged:
+                titleNotif = "Alerta de Batería"
+                messageNotif = f"⚡ Batería casi llena ({level} %). \nConsidera desconectar el cargador."
+                self.send_notification(titleNotif, messageNotif)
+                self.send_telegram_message(messageNotif)
+                self.play_notification_sound("battery-high.mp3")
+                
+                time.sleep(80)
         else:
-            messageNotif = f"ℹ️ El programa dejará de monitorear el nivel de batería ({level} %)."
-        send_telegram_message(messageNotif)
-    sys.exit(0)
-
-# Función general que revisa el estado de la batería
-def check_battery_level():
-    # Obtener información de la batería
-    battery = psutil.sensors_battery()
-    if battery is not None:
-        level = battery.percent
-        plugged = battery.power_plugged
+            print("No se pudo obtener información sobre la batería.")
+            exit()
         
-        # Verificar el nivel de la batería y si está conectada a la corriente
-        if level <= args['lower'] and not plugged:
-            titleNotif = "Alerta de Batería"
-            messageNotif = f"⚠️ Nivel de batería bajo ({level} %). \nConecta el cargador."
-            send_notification(titleNotif, messageNotif)
-            send_telegram_message(messageNotif)
-            play_notification_sound("battery-low.mp3")
-            
-            time.sleep(80)
-        elif level >= args['higher'] and plugged:
-            titleNotif = "Alerta de Batería"
-            messageNotif = f"⚡ Batería casi llena ({level} %). \nConsidera desconectar el cargador."
-            send_notification(titleNotif, messageNotif)
-            send_telegram_message(messageNotif)
-            play_notification_sound("battery-high.mp3")
-            
-            time.sleep(80)
-    else:
-        print("No se pudo obtener información sobre la batería.")
-        exit()
 
-# ciclo para revisar el nivel de la batería
-def main():
-    while True:
-        check_battery_level()
-        time.sleep(10)  # Espera 10 segundos antes de volver a comprobar
-
-if __name__ == "__main__":
-    # Configuramos el manejador para la señal SIGINT
-    signal.signal(signal.SIGINT, handle_interrupt)
-    signal.signal(signal.SIGTERM, handle_interrupt)
-    main()
+if __name__ == '__main__':
+    BatteryChecker.parse_command_line()
