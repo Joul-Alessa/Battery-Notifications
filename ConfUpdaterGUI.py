127753bfc936d028a56fa548a49665105466d0c5
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2025-07-19 00:56:53 -0600

    Finished layout to the GUI app

--- ConfUpdaterGUI.py
+++ ConfUpdaterGUI.py
@@ -13,11 +13,15 @@ class NotificationsApp(toga.App):
                 default_min_json = str(config.get("lower", 0))
                 default_max_json = str(config.get("higher", 100))
                 default_sound_enabled_json = config.get("sound", False)
+                default_closing_notif_json = config.get("closing", True)
+                default_msg_telegram_json = config.get("msgTelegram", False)
         except Exception as e:
             print("Error leyendo conf.json:", e)
             default_min_json = "0"
             default_max_json = "100"
             default_sound_enabled_json = False
+            default_closing_notif_json = True
+            default_msg_telegram_json = False
         
         # Contenedor principal
         main_box = toga.Box(style=Pack(direction=COLUMN, margin=10))
@@ -105,8 +109,6 @@ class NotificationsApp(toga.App):
 
         # Agregar fila a la ventana principal
         main_box.add(centered_row_2_box)
-        
-        main_box.add(centered_row_2_box)
 
         # Título 3
         title_3_label = toga.Label(
@@ -114,6 +116,90 @@ class NotificationsApp(toga.App):
             style=Pack(font_size=20, margin_top=10, margin_bottom=20, text_align="center", alignment="center")
         )
         main_box.add(title_3_label)
+
+        # Fila con para eventos misceláneos obligatorios
+        input_row_3 = toga.Box(
+            style=Pack(direction=ROW, gap=10, flex=1, alignment="center", justify_content="center")
+        )
+
+        closing_notif_label = toga.Label("Enable notifications when closing:", style=Pack(padding=(5, 0)))
+        self.closing_notif_checkbox = toga.Switch(
+            "",
+            style=Pack(),
+            value=default_closing_notif_json
+        )
+
+        telegram_label = toga.Label("Enable integration with Telegram:", style=Pack(padding=(5, 0)))
+        self.telegram_checkbox = toga.Switch(
+            "",
+            style=Pack(),
+            on_change=self.toggle_telegram_fields,
+            value=default_msg_telegram_json
+        )
+
+        input_row_3.add(closing_notif_label)
+        input_row_3.add(self.closing_notif_checkbox)
+        input_row_3.add(telegram_label)
+        input_row_3.add(self.telegram_checkbox)
+
+        # Envolver la fila en otro contenedor para centrarla
+        centered_row_3_box = toga.Box(
+            style=Pack(direction=ROW, justify_content="center")
+        )
+        centered_row_3_box.add(input_row_3)
+
+        # Agregar fila a la ventana principal
+        main_box.add(centered_row_3_box)
+
+        # Fila con para eventos misceláneos opcionales
+        self.input_row_4 = toga.Box(
+            style=Pack(direction=ROW, gap=10, flex=1, alignment="center", justify_content="center")
+        )
+
+        chat_id_label = toga.Label("Chat ID:", style=Pack(width=90))
+        self.chat_id_input = toga.TextInput(
+            style=Pack(width=100)
+        )
+
+        telegram_bot_label = toga.Label("Telegram Bot:", style=Pack(width=90))
+        self.telegram_bot_input = toga.TextInput(
+            style=Pack(width=100)
+        )
+
+        self.input_row_4.add(chat_id_label)
+        self.input_row_4.add(self.chat_id_input)
+        self.input_row_4.add(telegram_bot_label)
+        self.input_row_4.add(self.telegram_bot_input)
+
+        # Envolver la fila en otro contenedor para centrarla
+        self.centered_row_4_box = toga.Box(
+            style=Pack(direction=ROW, justify_content="center")
+        )
+        self.centered_row_4_box.add(self.input_row_4)
+
+        # Agregar fila a la ventana principal
+        main_box.add(self.centered_row_4_box)
+
+        # Fila de botones para guardar el contenido
+        input_row_5 = toga.Box(
+            style=Pack(direction=ROW, gap=10, flex=1, alignment="center", justify_content="center")
+        )
+
+        self.accept_button = toga.Button("Aceptar", on_press=self.on_accept, style=Pack(width=100))
+        self.cancel_button = toga.Button("Cancelar", on_press=self.on_cancel, style=Pack(width=100))
+
+        input_row_5.add(self.accept_button)
+        input_row_5.add(self.cancel_button)
+
+        # Envolver la fila en otro contenedor para centrarla
+        centered_row_5_box = toga.Box(
+            style=Pack(direction=ROW, justify_content="center")
+        )
+        centered_row_5_box.add(input_row_5)
+
+        # Agregar fila a la ventana principal
+        main_box.add(centered_row_5_box)
+
 
         # Mostrar ventana
         self.main_window = toga.MainWindow(title=self.formal_name)
@@ -133,6 +219,18 @@ class NotificationsApp(toga.App):
 
     def on_select_high(self, widget):
         print("High sound selected")
+    
+    def toggle_telegram_fields(self, widget):
+        if widget.value:
+            self.centered_row_4_box.add(self.input_row_4)
+        else:
+            self.centered_row_4_box.remove(self.input_row_4)
+    
+    def on_accept(self, widget):
+        print("on_accept")
+    
+    def on_cancel(self, widget):
+        print("on_cancel")
 
 
 def main():
